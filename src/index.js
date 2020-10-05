47368e0f066e6b393966a01952a71f65b4fa7076
Aleksandr <70815958+mymage259@users.noreply.github.com>
2020-10-06 00:50:21 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);
+  let box = String(n);
+  let box2 = '';
+  for (let i = box.length - 1; i >= 0; i--) {
+    box2 += box[i];
+  }
+return Number(box2);
 }
